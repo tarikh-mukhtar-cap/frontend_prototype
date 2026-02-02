4470930ed4fae9cb387395f80f66a973d69b9d6b
tarikh-mukhtar-cap <tarikh.mukhtar@capgemini.com>
2026-02-02 15:20:46 +0000

    setup routing

--- app/routes.js
+++ app/routes.js
@@ -7,3 +7,22 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
+router.get('/', function (req,res) {
+    res.render('index', { page: 'about' })
+})
+
+router.get('/blog', function (req,res) {
+    res.render('index', { page: 'blog' })
+})
+
+router.get('/chat', function (req,res) {
+    res.render('index', { page: 'chat' })
+})
+
+router.get('/resources', function (req,res) {
+    res.render('index', { page: 'resources' })
+})
+
+router.get('/links', function (req,res) {
+    res.render('index', { page: 'links' })
+})
